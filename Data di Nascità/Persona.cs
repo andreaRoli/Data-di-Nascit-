f947a56c740a96fe5863fc1391e966e1163f7c81
andreaRoli <aggiustatuttobob4@gmail.com>
2024-01-23 15:17:16 +0100

    Aggiungere i file di progetto.

---
+++ Data di Nascità/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_di_Nascità
+{
+    internal class Persona
+    { 
+        public DateTime DataDiNascità { get; set; }
+
+        public int CalcolaEtà()
+        {
+            int Anni=DateTime.Now.Year-DataDiNascità.Year;
+           if(DateTime.Now.Day>=DataDiNascità.Day&&DateTime.Now.Month<DataDiNascità.Month || DateTime.Now.Day < DataDiNascità.Day && DateTime.Now.Month>=DataDiNascità.Month || DateTime.Now.Day<DataDiNascità.Day && DateTime.Now.Month<DataDiNascità.Month)
+            {
+                Anni = Anni - 1;
+                return Anni;
+            }
+            else
+            {
+                return Anni;
+            }
+        }
+    }
+}
---
+++ Data di Nascità/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_di_Nascità
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p=new Persona();
+            Console.WriteLine("Quando sei nato?");
+            p.DataDiNascità=DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ora hai {0} anni", p.CalcolaEtà());
+            Console.ReadLine();
+        }
+    }
+}
